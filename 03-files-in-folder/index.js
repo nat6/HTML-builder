d25b54662f078b571e553f029e1599c6feddee92
nat6 <nat6dv@gmail.com>
2021-11-08 22:07:27 +0300

    feat: add readdir

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'secret-folder');
+
+fs.readdir(folder, { withFileTypes: true }, (err, data) => {
+  data.forEach((file) => {
+    if (file.isFile()) {
+      const current = file.name;
+      const ext = path.extname(current).substr(1);
+      const name = current
+        .split('.')
+        .splice(this.length - 1, 1)
+        .join('');
+
+      fs.stat(folder + '/' + current, (err, data) => {
+        const size = data.size / 1000 + 'kb';
+        console.log(name + ' - ' + ext + ' - ' + size);
+      });
+    }
+  });
+});
