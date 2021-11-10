bb668c52f7b8eb83a4b56eccdca1bb00e224c7be
nat6 <nat6dv@gmail.com>
2021-11-10 20:58:10 +0300

    feat: add bundle functions

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,4 +1,50 @@
-console.log(
-  `Добрый день! Не успела доделать 5 и 6 задачу. Если не сложно, оставьте, пожалуйста, контакты (либо проверьте в четверг). 
-Спасибо :)`
-);
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
+
+const src = path.join(__dirname, 'styles');
+const output = path.join(__dirname, 'project-dist', 'bundle.css');
+
+async function filterFiles(src, ext) {
+  let files = await readdir(src, { withFileTypes: true });
+  const cssFiles = [];
+
+  files.forEach((file) => {
+    const current = file.name;
+    const currentExt = path.extname(current).substr(1);
+    if (file.isFile() && currentExt === ext) {
+      cssFiles.push(current);
+    }
+  });
+
+  return cssFiles;
+}
+
+async function readFiles() {
+  const cssFiles = await filterFiles(src, 'css');
+  let data = [];
+
+  for (const file of cssFiles) {
+    const currentPath = path.resolve(src, file);
+
+    let ReadStream = fs.createReadStream(currentPath, 'utf8');
+    for await (const chunk of ReadStream) {
+      data.push(chunk);
+    }
+  }
+
+  return data;
+}
+
+async function writeFiles() {
+  const cssArr = await readFiles();
+
+  let writeableStream = fs.createWriteStream(output);
+
+  cssArr.forEach((item) => {
+    writeableStream.write(`${item}\n`);
+  });
+  console.log('я сделяль!');
+}
+
+writeFiles();
